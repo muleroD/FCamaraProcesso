31a88264bb743d82ef1e855782a3b94530d4947d
Richard Mulero <mulerolp@gmail.com>
2018-08-23 21:47:49 -0300

    testes fcamara

--- src/exercicio4/Teste4.java
+++ src/exercicio4/Teste4.java
@@ -1,10 +1,11 @@
 /*
-Faça um programa para uma loja de tintas. O programa deverá pedir o
-tamanho em metros quadrados da área a ser pintada. Considere que a
-cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é
-vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a
-quantidades de latas de tinta a serem compradas e o preço total. */
+Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. 
+Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é
+vendida em latas de 18 litros, que custam R$ 80,00. 
+Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total. */
 package exercicio4;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,4 +13,28 @@ package exercicio4;
  */
 public class Teste4 {
 
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        double precoLata = 80, precoTotal, qtLatas, coberturaTinta, metrosQuad;
+
+        System.out.print("Informe o tamanho em metros quadrados da área a ser pintada: ");
+        metrosQuad = read.nextInt();
+
+        coberturaTinta = metrosQuad / 3;
+
+        System.out.println(Math.round(coberturaTinta) + "L de tinta será utilizado");
+
+        if (coberturaTinta < 19) {
+            System.out.println("1 lata de tinta");
+            System.out.println("Total: \tR$" + precoLata);
+
+        } else {
+            qtLatas = coberturaTinta / 18;
+            precoTotal = qtLatas * precoLata;
+            System.out.println(Math.round(qtLatas) + " latas de tinta");
+            System.out.println("Total: \tR$" + Math.round(precoTotal));
+        }
+    }
 }
